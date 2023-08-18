PixelExperience/build

build: Guard more lines related to AOSP ringtones
Change-Id: Ib01ae9abdf1b8fe9ece29485e9bc9820d0af48d9

target/product/full_base.mk
@@ -43,9 +43,11 @@ PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
 # Additional settings used in all AOSP builds
+ifeq ($(CUSTOM_BUILD),)
 PRODUCT_VENDOR_PROPERTIES := \
     ro.config.ringtone?=Ring_Synth_04.ogg \
     ro.config.notification_sound?=pixiedust.ogg
+endif
 
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
@@ -53,8 +55,6 @@ PRODUCT_LOCALES := en_US
 # Get some sounds
 ifeq ($(CUSTOM_BUILD),)
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
-else
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage14.mk)
 endif
 
 # Get a list of languages.

target/product/handheld_system.mk
@@ -83,6 +83,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libeffects/data/audio_effects.conf:system/etc/audio_effects.conf
 
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.carrier?=unknown \
+    ro.carrier?=unknown
+
+ifeq ($(CUSTOM_BUILD),)
+PRODUCT_VENDOR_PROPERTIES := \
     ro.config.notification_sound?=OnTheHunt.ogg \
     ro.config.alarm_alert?=Alarm_Classic.ogg
+endif

HavocOS-Android11/android_build

Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

target/product/full_base.mk
@@ -37,9 +37,11 @@ PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
 # Additional settings used in all AOSP builds
+ifeq ($(LINEAGE_BUILD),)
 PRODUCT_VENDOR_PROPERTIES := \
     ro.config.ringtone?=Ring_Synth_04.ogg \
     ro.config.notification_sound?=pixiedust.ogg
+endif
 
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
@@ -47,8 +49,6 @@ PRODUCT_LOCALES := en_US
 # Get some sounds
 ifeq ($(LINEAGE_BUILD),)
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
-else
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage14.mk)
 endif
 
 # Get a list of languages.

target/product/handheld_system.mk
@@ -70,7 +70,6 @@ PRODUCT_PACKAGES += \
     VpnDialogs \
     vr \
 
-
 PRODUCT_SYSTEM_SERVER_APPS += \
     FusedLocation \
     InputDevices \
@@ -81,9 +80,13 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libeffects/data/audio_effects.conf:system/etc/audio_effects.conf
 
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.carrier?=unknown \
+    ro.carrier?=unknown
+
+ifeq ($(LINEAGE_BUILD),)
+PRODUCT_VENDOR_PROPERTIES := \
     ro.config.notification_sound?=OnTheHunt.ogg \
     ro.config.alarm_alert?=Alarm_Classic.ogg
+endif
 
 # Traceur for debug only
 PRODUCT_PACKAGES_ENG += \